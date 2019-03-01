25d242c2b7556c7607d7fe8a717a862c2c8aa8a4
Ante Bozic <ante@Antes-MacBook-Pro.local>
2019-03-01 23:30:11 +0100

    Updates

---
+++ precache-manifest.40dd4bdffa03b4c58619faeff9b4ed0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbabce73dca862448c6d",
+    "url": "/country-flag/static/js/runtime~main.dbabce73.js"
+  },
+  {
+    "revision": "48f511b7bc082b66a912",
+    "url": "/country-flag/static/js/main.48f511b7.chunk.js"
+  },
+  {
+    "revision": "7e0eff7825c9be5aab89",
+    "url": "/country-flag/static/js/1.7e0eff78.chunk.js"
+  },
+  {
+    "revision": "48f511b7bc082b66a912",
+    "url": "/country-flag/static/css/main.84f3b1ba.chunk.css"
+  },
+  {
+    "revision": "b1469b167fb7074ad1a7bbea291c7281",
+    "url": "/country-flag/index.html"
+  }
+];
