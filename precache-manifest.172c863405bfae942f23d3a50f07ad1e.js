0aa1942ff5b1ecccf76ab09a28834dffc7e7742b
Ante Bozic <ante@Antes-MacBook-Pro.local>
2019-03-02 16:10:20 +0100

    Updates

--- precache-manifest.172c863405bfae942f23d3a50f07ad1e.js
+++ precache-manifest.172c863405bfae942f23d3a50f07ad1e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/country-flag/static/js/runtime~main.dbabce73.js"
   },
   {
-    "revision": "174998e7aef34da2f97c",
-    "url": "/country-flag/static/js/main.174998e7.chunk.js"
+    "revision": "a4616b9fdc7b3e02e5d2",
+    "url": "/country-flag/static/js/main.a4616b9f.chunk.js"
   },
   {
     "revision": "6f507d9de21f9a669927",
     "url": "/country-flag/static/js/1.6f507d9d.chunk.js"
   },
   {
-    "revision": "174998e7aef34da2f97c",
-    "url": "/country-flag/static/css/main.f975e875.chunk.css"
+    "revision": "a4616b9fdc7b3e02e5d2",
+    "url": "/country-flag/static/css/main.9ec1fdc6.chunk.css"
   },
   {
-    "revision": "8fa98e5133dba0e4a627259217091ea0",
+    "revision": "c4c79876fca5d096a0621affa1acd1e7",
     "url": "/country-flag/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/country-flag/precache-manifest.668cb9353b22be7c7ea6ca44c3e52f36.js"
+  "/country-flag/precache-manifest.172c863405bfae942f23d3a50f07ad1e.js"
 );
 
 workbox.clientsClaim();
